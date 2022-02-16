5ff55a92c17c070441c756da0ed92a373db38bbf
Kauan Souza <k1one.g.vitor@gmail.com>
2022-02-16 11:09:57 -0300

    Created method findById

--- backend/src/main/java/com/devsuperior/dsclient/resource/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resource/ClientResource.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,8 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 
-
+	
+	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
@@ -31,10 +34,15 @@ public class ClientResource {
 			
 			Page<ClientDTO> list = service.findAllPaged(pageRequest);
 			return ResponseEntity.ok(list);
-		
 	}
-
-
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 
 
 
