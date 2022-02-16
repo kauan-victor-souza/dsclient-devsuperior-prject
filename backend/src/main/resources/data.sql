09b042aad198640d907e0734acc907062055a6a7
Kauan Souza <k1one.g.vitor@gmail.com>
2022-02-16 14:57:46 -0300

    Created database data.sql

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Beatriz', 3000.0,TIMESTAMP WITH TIME ZONE '1997-07-07T10:00:00Z', 1);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Kauan', 2000.0,TIMESTAMP WITH TIME ZONE '1995-11-20T10:00:00Z', 1);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Lucas', 7000.0,TIMESTAMP WITH TIME ZONE '1990-10-02T10:00:00Z', 2);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Michele', 1000.0,TIMESTAMP WITH TIME ZONE '1992-01-05T10:00:00Z', 4);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Ricardo', 5000.0,TIMESTAMP WITH TIME ZONE '1993-07-11T10:00:00Z', 2);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Manuel', 2800.0,TIMESTAMP WITH TIME ZONE '1999-09-16T10:00:00Z', 3);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Paulo', 800.0,TIMESTAMP WITH TIME ZONE '2000-10-30T10:00:00Z', 7);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Cassio', 3800.0,TIMESTAMP WITH TIME ZONE '2002-12-21T10:00:00Z', 6);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Efrain', 10000.0,TIMESTAMP WITH TIME ZONE '1989-05-22T10:00:00Z', 3);
+INSERT INTO tb_client (name, income, birth_date, children) VALUES ('Larissa', 700.0,TIMESTAMP WITH TIME ZONE '1998-09-25T10:00:00Z', 2);
